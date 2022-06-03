fddbb14ee0552ea4d86e6ee97223720100f7da09
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-03 15:06:07 +0530

    [shravanthi] -Load the Indian State Census Information(Count of Rows)

--- IndianStateCensus_Analyser/Program.cs
+++ IndianStateCensus_Analyser/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CsvHelper;
+using CsvHelper.Configuration;
+using IndianStateCensus_Analyser;
+using IndianStateCensus_Analyser.POCO;
+using System.Globalization;
+using static IndianStateCensus_Analyser.CensusAnalyser;
+
 Console.WriteLine("Hello, Welcome to Indian State Census Analyser!");
+CensusAnalyser censusAnalyser = new();
+string csvPath = @"C:\Users\Admin\Desktop\CSharp\IndianStateCensusAnalyzer\IndianStateCensus_Analyser\IndiaStateCensusData.csv";
+//censusAnalyser.ReadCsvFile();
+string IndianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
+Dictionary<string, CensusDTO> totalRecord = censusAnalyser.LoadCensusData(Country.INDIA, csvPath, IndianStateCensusHeaders);
+Console.WriteLine(totalRecord.Count);
